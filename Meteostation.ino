efcc129a030ea0501bb6e2d0601e0a98040de279
TheMiner203 <84179454+TheMiner203@users.noreply.github.com>
2023-07-23 20:17:06 +0800

    Последнее обновление
    Экран ошибки стал информативней; не нужна перезагрузка для подключения модуля

--- Meteostation.ino
+++ Meteostation.ino
@@ -1,11 +1,15 @@
 // Метеостанция
 // OLED SCL - A5
 // OLED SDA - A4
-#define DHTPIN A3
+#define DHTPIN A3       // Пин, к которому подключен датчик
+#define DHTMODEL DHT11  // Модель датчика (DHT11 или DHT22)
 #include "DHT.h"
 #include <GyverOLED.h>
 GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
-DHT dht(DHTPIN, DHT11);
+DHT dht(DHTPIN, DHTMODEL);
+bool isError;
+float t;
+float h;
 
 void setup() {
   pinMode(13, OUTPUT);
@@ -20,38 +24,52 @@ void setup() {
   oled.setCursor(0, 4);
   oled.print("станция");
   delay(1500);
+  start();
+}
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isError == true && (!isnan(h) || !isnan(t))) {
+    isError = false;
+    oled.clear();
+    start();
+  }
+  if (isnan(h) || isnan(t)) {
+    if (isError == false) {
+      oled.clear();
+    }
+    oled.home();
+    oled.setScale(2);
+    oled.print("Ошибка");
+    oled.setScale(1);
+    oled.setCursor(0, 3);
+    oled.print("Подключите датчик");
+    oled.setCursor(0, 5);
+    oled.print("DHT");
+    oled.print(DHTMODEL);
+    oled.print(" к пину ");
+    oled.print(DHTPIN);
+    oled.setScale(1);
+    isError = true;
+  }
+  if (isError == false) {
+    Serial.print(t);
+    Serial.print(',');
+    Serial.println(h);
+    oled.setCursorXY(60, 0);
+    oled.print(t, 1);
+    oled.print("C");
+    oled.setCursorXY(60, 40);
+    oled.print(h, 1);
+    oled.print("%");
+    delay(2000);
+  }
+}
+void start() {
   oled.clear();
   oled.home();
   oled.setScale(2);
   oled.print("Темп:");
   oled.setCursor(0, 5);
   oled.print("Влаж: ");
-}
-void loop() {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  if (isnan(h) || isnan(t)) {
-    oled.clear();
-    oled.home();
-    oled.print("Ошибка");
-    oled.setScale(1);
-    oled.setCursor(0, 3);
-    oled.print("Подключите датчик и");
-    oled.setCursor(0, 5);
-    oled.print("перезагрузите устрой-");
-    oled.setCursor(0, 7);
-    oled.print("ство");
-    for(;;);
-    return;
-  }
-  Serial.print(t);
-  Serial.print(',');
-  Serial.println(h);
-  oled.setCursorXY(60, 0);
-  oled.print(t, 1);
-  oled.print("C");
-  oled.setCursorXY(60, 40);
-  oled.print(h, 1);
-  oled.print("%");
-  delay(2000);
 }
